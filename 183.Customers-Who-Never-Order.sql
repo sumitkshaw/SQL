b6901c870264feee81dd9ab6b2228c1380d6c0c9
sumitkshaw <shawsumit6286@gmail.com>
2025-07-31 10:33:53 +0530

    sql4-changes

--- 183.Customers-Who-Never-Order.sql
+++ 183.Customers-Who-Never-Order.sql
@@ -1,3 +1,8 @@
 SELECT name as customers
 FROM customers
 WHERE id not in (SELECT customerid FROM orders);
+
+--- SELECT c.name AS Customers
+--- FROM Customers C
+--- LEFT JOIN Orders o ON c.id = o.customerId
+--- WHERE o.id IS NULL
