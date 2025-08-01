bc12c228ee3a9f75f087734d9348718c8e990669
sumitkshaw <shawsumit6286@gmail.com>
2025-08-01 16:23:15 +0530

    sql-7

--- 184.Department-Highest-Salary.sql
+++ 184.Department-Highest-Salary.sql
@@ -1,7 +1,7 @@
 SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
 FROM Employee e    
 JOIN Department d ON e.departmentId = d.id 
-WHERE e.salary = (
+WHERE e.salary = (              --= calculate max salary for each department
     SELECT MAX(salary)
     FROM Employee
     WHERE departmentId = e.departmentId
